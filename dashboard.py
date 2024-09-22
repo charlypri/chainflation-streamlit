3dd82491e8f3e26e0d1a346a5d91a562fbfd19e3
Carlos <charlypri@gmail.com>
2024-09-23 01:30:42 +0200

    Revamp dashboard to alerts.py

--- dashboard.py
+++ dashboard.py
@@ -12,23 +12,23 @@ from datetime import datetime, time, date
 from streamlit_option_menu import option_menu
 from functions import *
 
-st.set_page_config(page_title="Chainflation - Dashboard",layout="wide")
+st.set_page_config(page_title="Chainflation - Dashboard", layout="wide")
 st.title("Chainflation Data Sources")
 
 
-@st.cache(hash_funcs={"pymongo.mongo_client.MongoClient": id}) 
+@st.cache(hash_funcs={"pymongo.mongo_client.MongoClient": id})
 def loadData():
 
     # get product prices
-    prod_prices  = getProductPrices()
+    prod_prices = getProductPrices()
 
     # # Get inflation per soruce
     sources_infl = getSourcesInflation(prod_prices, 30)
 
     # Get inflation per product
-    prod_infl = getProductInflation(prod_prices, 30) 
+    prod_infl = getProductInflation(prod_prices, 30)
 
-    # # Get inflation per category 
+    # # Get inflation per category
     category_infl = getCategoriesInflation(prod_infl)
 
     # Get total inflation
@@ -36,10 +36,15 @@ def loadData():
 
     return prod_prices, sources_infl, prod_infl, category_infl, total
 
+
 def plot_priceTime(prod_params):
 
-    
-    fig = px.line(prod_prices[(prod_prices['fuente'] == prod_params)], x="fecha", y="precio", color= "producto")
+    fig = px.line(
+        prod_prices[(prod_prices["fuente"] == prod_params)],
+        x="fecha",
+        y="precio",
+        color="producto",
+    )
     fig.update_layout(
         title=f"Precios {prod_params}",
         xaxis_title="Fecha",
@@ -47,26 +52,36 @@ def plot_priceTime(prod_params):
     )
     return fig
 
+
 def plot_prod_infTime():
-    sums = sources_infl.groupby(["fecha","fuente"]).mean(numeric_only=True).reset_index()
+    sums = (
+        sources_infl.groupby(["fecha", "fuente"]).mean(numeric_only=True).reset_index()
+    )
     if param_cesta == "alimentacion":
-        sums = sums[(sums["inflation"]<15) & (sums["inflation"]> -15)]
-    fig = px.line(sums, x="fecha", y="inflation", color='fuente')
+        sums = sums[(sums["inflation"] < 15) & (sums["inflation"] > -15)]
+    fig = px.line(sums, x="fecha", y="inflation", color="fuente")
     fig.update_layout(
         xaxis_title="Fecha",
         yaxis_title="Precio",
     )
     return fig
 
+
 def plot_prod_comTime(prod_params):
 
-    fig = px.line(prod_prices[(prod_prices['producto'] == prod_params)], x="fecha", y="precio", color='fuente')
+    fig = px.line(
+        prod_prices[(prod_prices["producto"] == prod_params)],
+        x="fecha",
+        y="precio",
+        color="fuente",
+    )
     fig.update_layout(
         title=f"Precio por supermercado",
         xaxis_title="Fecha",
         yaxis_title="Precio",
     )
     return fig
+
 
 def filter_df_by_date(df, time_period):
     # Get current date and time
@@ -77,36 +92,38 @@ def filter_df_by_date(df, time_period):
         date_last_week = now - timedelta(days=7)
         # Filter values for the last week
         filtered_df = df[df["fecha"] >= date_last_week]
-        
+
     elif time_period == "Mes":
         # Get date for a month ago
         date_last_month = now - timedelta(days=30)
         # Filter values for the last month
         filtered_df = df[df["fecha"] >= date_last_month]
-    
+
     elif time_period == "Trimestre":
         # Get date for a year ago
         date_last_year = now - timedelta(days=90)
         # Filter values for the last year
         filtered_df = df[df["fecha"] >= date_last_year]
-        
+
     elif time_period == "Año":
         # Get date for a year ago
         date_last_year = now - timedelta(days=365)
         # Filter values for the last year
         filtered_df = df[df["fecha"] >= date_last_year]
-        
+
     else:
-        raise ValueError("Invalid time_period argument. Possible values: 'last_week', 'last_month', 'last_year'.")
-        
+        raise ValueError(
+            "Invalid time_period argument. Possible values: 'last_week', 'last_month', 'last_year'."
+        )
+
     return filtered_df
 
 
 with st.sidebar:
     selected = option_menu(
         "",
-        ["Inflación", 'Sectores'],
-        icons=["","bi-joystick"],
+        ["Inflación", "Sectores"],
+        icons=["", "bi-joystick"],
         menu_icon="",
         default_index=0,
     )
@@ -119,10 +136,14 @@ if selected == "Sectores":
     # SECCION DE PRECIOS SEGUN SECTOR Y SOURCES
     # PARAMETROS
     st.subheader(f"Histórico de precios por sector:")
-    col1, col2, col3, = st.columns(3)
+    (
+        col1,
+        col2,
+        col3,
+    ) = st.columns(3)
     with col1:
         param_cesta = st.selectbox(
-            "Selecciona el sector", ['alimentacion', 'energia', 'vivienda']
+            "Selecciona el sector", ["alimentacion", "energia", "vivienda"]
         )
 
     prod_prices = prods_prices[param_cesta]
@@ -130,47 +151,33 @@ if selected == "Sectores":
     prod_infl = prods_infl[param_cesta]
     category_infl = categories_infl[param_cesta]
 
-    sources_names = prod_prices['fuente'].unique()
-    product_names = prod_prices['producto'].unique()
-    
+    sources_names = prod_prices["fuente"].unique()
+    product_names = prod_prices["producto"].unique()
 
     with col2:
-        param_prod = st.selectbox(
-            "Selecciona la fuente", 
-            sources_names
-        )
-    
+        param_prod = st.selectbox("Selecciona la fuente", sources_names)
+
     with col3:
         periodo = st.selectbox(
-            "Selecciona el periodo", 
-            ["Semana", "Mes", "Trimestre", "Año"]
+            "Selecciona el periodo", ["Semana", "Mes", "Trimestre", "Año"]
         )
-    
-    
+
     prod_prices = filter_df_by_date(prod_prices, periodo)
-    
+
     fig = plot_priceTime(param_prod)
     st.plotly_chart(fig)
 
     if param_cesta == "alimentacion":
-        st.subheader(
-            f"Compara precios entre supermercados:"
-        )
-        param_prod = st.selectbox(
-            "Selecciona el producto a comparar", 
-            product_names
-        )
+        st.subheader(f"Compara precios entre supermercados:")
+        param_prod = st.selectbox("Selecciona el producto a comparar", product_names)
         fig = plot_prod_comTime(param_prod)
         st.plotly_chart(fig)
 
     # SECCION DE INFLACION
-    st.subheader(
-        f"Inflación por fuente:"
-    )
+    st.subheader(f"Inflación por fuente:")
 
     fig = plot_prod_infTime()
     st.plotly_chart(fig)
-
 
 
 if selected == "Inflación":
@@ -180,10 +187,16 @@ if selected == "Inflación":
 
     fig = go.Figure()
 
-    for category in total['category'].unique():
-        category_data = total[total['category'] == category]
-        fig.add_trace(go.Scatter(x=category_data['fecha'], y=category_data['inflation'], 
-                                mode='lines+markers', name=category))
+    for category in total["category"].unique():
+        category_data = total[total["category"] == category]
+        fig.add_trace(
+            go.Scatter(
+                x=category_data["fecha"],
+                y=category_data["inflation"],
+                mode="lines+markers",
+                name=category,
+            )
+        )
 
     fig.update_layout(
         title=f"Inflación por productos",
--- functions.py
+++ functions.py
@@ -1,6 +1,4 @@
 import pandas as pd
-import plotly.express as px
-import matplotlib.pyplot as plt
 
 import pymongo
 from datetime import datetime, time, timedelta
@@ -12,146 +10,204 @@ pd.set_option("display.max_columns", None)
 DATABASE = "chainflation"
 year_date = datetime.now() - timedelta(days=365)
 
+
 def getAlimentacionJson():
-    myclient = pymongo.MongoClient(os.environ['chainflation_mongo'])
+    myclient = pymongo.MongoClient(os.environ["chainflation_mongo"])
     mydb = myclient[DATABASE]
 
     alim_collect = mydb["alimentacion"]
-    cursor  = alim_collect.find({"fecha": {"$gte": year_date}},{"_id":0, 'fecha': 1, 'producto': 1, 'precio_referencia': 1, 'tienda': 1}).sort("fecha", 1)
+    cursor = alim_collect.find(
+        {"fecha": {"$gte": year_date}},
+        {"_id": 0, "fecha": 1, "producto": 1, "precio_referencia": 1, "tienda": 1},
+    ).sort("fecha", 1)
     records = list(cursor)
-    
+
     return records
 
+
 def getViviendaJson():
-    myclient = pymongo.MongoClient(os.environ['chainflation_mongo'])
+    myclient = pymongo.MongoClient(os.environ["chainflation_mongo"])
     mydb = myclient[DATABASE]
 
     alim_collect = mydb["vivienda"]
-    cursor  = alim_collect.find({"fecha": {"$gte": year_date}},{"_id":0, 'fecha': 1, 'tipo': 1, 'provincia':1, 'precio': 1, 'fuente': 1}).sort("fecha", 1)
+    cursor = alim_collect.find(
+        {"fecha": {"$gte": year_date}},
+        {"_id": 0, "fecha": 1, "tipo": 1, "provincia": 1, "precio": 1, "fuente": 1},
+    ).sort("fecha", 1)
     records = list(cursor)
-    
+
     return records
+
 
 def getEnergiaJson():
-    myclient = pymongo.MongoClient(os.environ['chainflation_mongo'])
+    myclient = pymongo.MongoClient(os.environ["chainflation_mongo"])
     mydb = myclient[DATABASE]
-    
+
     alim_collect = mydb["energia"]
-    cursor  = alim_collect.find({"fecha": {"$gte": year_date}},{"_id":0, 'fecha': 1, 'combustible': 1,  'precio': 1, 'fuente': 1}).sort("fecha", 1)
+    cursor = alim_collect.find(
+        {"fecha": {"$gte": year_date}},
+        {"_id": 0, "fecha": 1, "combustible": 1, "precio": 1, "fuente": 1},
+    ).sort("fecha", 1)
     records = list(cursor)
-    
+
     return records
 
+
 def calcInflationFuentes(sector_df, ago):
-    sector_df.fecha = sector_df.fecha.apply(lambda x: datetime(x.year, x.month, x.day)) # Keep only Year,Month,Day
+    sector_df.fecha = sector_df.fecha.apply(
+        lambda x: datetime(x.year, x.month, x.day)
+    )  # Keep only Year,Month,Day
 
     total_days = len(sector_df.fecha.unique())
-    
-    record_date = sector_df.fecha.max() #last day recorded
-    ref_date =(record_date - timedelta(days=ago)) # Day to compare with
+
+    record_date = sector_df.fecha.max()  # last day recorded
+    ref_date = record_date - timedelta(days=ago)  # Day to compare with
     inflations_products = pd.DataFrame()
     while total_days > 0:
-        
-        end_prices = sector_df[sector_df['fecha'] == record_date].groupby(["producto","fuente", "fecha"]).mean(numeric_only=True).sort_values(by=['producto']).reset_index()
-        start_prices = sector_df[sector_df['fecha'] == ref_date].groupby(["producto","fuente", "fecha"]).mean(numeric_only=True).sort_values(by=['producto']).reset_index()
+
+        end_prices = (
+            sector_df[sector_df["fecha"] == record_date]
+            .groupby(["producto", "fuente", "fecha"])
+            .mean(numeric_only=True)
+            .sort_values(by=["producto"])
+            .reset_index()
+        )
+        start_prices = (
+            sector_df[sector_df["fecha"] == ref_date]
+            .groupby(["producto", "fuente", "fecha"])
+            .mean(numeric_only=True)
+            .sort_values(by=["producto"])
+            .reset_index()
+        )
 
         if len(end_prices) == 0:
-            record_date =(record_date - timedelta(days=1))
+            record_date = record_date - timedelta(days=1)
             continue
         elif len(start_prices) == 0:
-            ref_date =(ref_date - timedelta(days=1))
+            ref_date = ref_date - timedelta(days=1)
             total_days -= 1
             continue
-        prices = pd.merge(start_prices, end_prices, on=['producto', "fuente"], how='inner', suffixes=('_ref', ''))
-        prices["inflation"] = ((prices["precio"] - prices["precio_ref"]) / prices["precio_ref"]) *100
-        inflations_products = pd.concat([inflations_products, prices], ignore_index=True)
-        
-        record_date = (record_date - timedelta(days=1))
-        ref_date =(record_date - timedelta(days=ago))
+        prices = pd.merge(
+            start_prices,
+            end_prices,
+            on=["producto", "fuente"],
+            how="inner",
+            suffixes=("_ref", ""),
+        )
+        prices["inflation"] = (
+            (prices["precio"] - prices["precio_ref"]) / prices["precio_ref"]
+        ) * 100
+        inflations_products = pd.concat(
+            [inflations_products, prices], ignore_index=True
+        )
+
+        record_date = record_date - timedelta(days=1)
+        ref_date = record_date - timedelta(days=ago)
         total_days -= 1
 
-    inflations_products.drop(["precio_ref","fecha_ref"], axis=1, inplace=True)
+    inflations_products.drop(["precio_ref", "fecha_ref"], axis=1, inplace=True)
     return inflations_products
 
 
 def calcInflationProds(sector_df, ago):
-    sector_df.fecha = sector_df.fecha.apply(lambda x: datetime(x.year, x.month, x.day)).copy() # Keep only Year,Month,Day
-    
-    record_date = sector_df.fecha.max() #last day recorded
-    ref_date =(record_date - timedelta(days=ago)) # Day to compare with
-    inflations_products = pd.DataFrame()
-    while (ref_date >= sector_df.fecha.min()):
+    sector_df.fecha = sector_df.fecha.apply(
+        lambda x: datetime(x.year, x.month, x.day)
+    ).copy()  # Keep only Year,Month,Day
 
-        record_prices = sector_df[sector_df['fecha'] == record_date].groupby(["producto", "fecha"]).mean(numeric_only=True).sort_values(by=['producto']).reset_index()
-        ref_prices = sector_df[sector_df['fecha'] == ref_date].groupby(["producto", "fecha"]).mean(numeric_only=True).sort_values(by=['producto']).reset_index()
+    record_date = sector_df.fecha.max()  # last day recorded
+    ref_date = record_date - timedelta(days=ago)  # Day to compare with
+    inflations_products = pd.DataFrame()
+    while ref_date >= sector_df.fecha.min():
+
+        record_prices = (
+            sector_df[sector_df["fecha"] == record_date]
+            .groupby(["producto", "fecha"])
+            .mean(numeric_only=True)
+            .sort_values(by=["producto"])
+            .reset_index()
+        )
+        ref_prices = (
+            sector_df[sector_df["fecha"] == ref_date]
+            .groupby(["producto", "fecha"])
+            .mean(numeric_only=True)
+            .sort_values(by=["producto"])
+            .reset_index()
+        )
 
         if len(record_prices) == 0:
-            record_date =(record_date - timedelta(days=1))
-            ref_date =(record_date - timedelta(days=ago))
+            record_date = record_date - timedelta(days=1)
+            ref_date = record_date - timedelta(days=ago)
             continue
         elif len(ref_prices) == 0:
-            ref_date =(ref_date - timedelta(days=1)) 
+            ref_date = ref_date - timedelta(days=1)
             continue
 
-        prices = pd.merge(ref_prices, record_prices, on='producto', how='inner', suffixes=('_ref', ''))
-        prices["inflation"] = ((prices["precio"] - prices["precio_ref"]) / prices["precio_ref"]) *100
-        inflations_products = pd.concat([inflations_products, prices], ignore_index=True)
-        
-        record_date = (record_date - timedelta(days=1))
-        ref_date =(record_date - timedelta(days=ago))
+        prices = pd.merge(
+            ref_prices, record_prices, on="producto", how="inner", suffixes=("_ref", "")
+        )
+        prices["inflation"] = (
+            (prices["precio"] - prices["precio_ref"]) / prices["precio_ref"]
+        ) * 100
+        inflations_products = pd.concat(
+            [inflations_products, prices], ignore_index=True
+        )
+
+        record_date = record_date - timedelta(days=1)
+        ref_date = record_date - timedelta(days=ago)
 
     return inflations_products
-
 
 
 def calcCategoriesInflation(inflations_product, sector):
     categ_inflations = []
     for date in inflations_product.fecha.unique():
-        daily_inflations = inflations_product[inflations_product['fecha'] == date]['inflation'].values
+        daily_inflations = inflations_product[inflations_product["fecha"] == date][
+            "inflation"
+        ].values
 
         pesos = {
-            'alimentacion': [   0.05882, # Aceite
-                0.05882, # mantequilla
-                0.05882, # arroz
-                0.05882, # maiz
-                0.05882, # garbanzos
-                0.05882, # alubias
-                0.05882, # patata
-                0.05882, # azucar
-                0.05882, # pescado
-                0.05882, # huevos
-                0.05882, # pollo
-                0.05882, # leche
-                0.05882, # agua
-                0.05882, # cafe
-                0.05882, # cerveza
-                0.05882, # platano
-                0.05882, # manzana
+            "alimentacion": [
+                0.05882,  # Aceite
+                0.05882,  # mantequilla
+                0.05882,  # arroz
+                0.05882,  # maiz
+                0.05882,  # garbanzos
+                0.05882,  # alubias
+                0.05882,  # patata
+                0.05882,  # azucar
+                0.05882,  # pescado
+                0.05882,  # huevos
+                0.05882,  # pollo
+                0.05882,  # leche
+                0.05882,  # agua
+                0.05882,  # cafe
+                0.05882,  # cerveza
+                0.05882,  # platano
+                0.05882,  # manzana
             ],
-            'energia': [   
-                0.1, # Sin Plomo 95
-                0.1, # Sin Plomo 98
-                0.1, # Gasóleo A
-                0.1, # Gasóleo A+
-                0.1, # Gasóleo B
-                0.1, # Gasóleo C
-                0.1, # Biodiésel
-                0.1, # Autogas/GLP
-                0.1, # GNC
-                0.1 # Luz
+            "energia": [
+                0.1,  # Sin Plomo 95
+                0.1,  # Sin Plomo 98
+                0.1,  # Gasóleo A
+                0.1,  # Gasóleo A+
+                0.1,  # Gasóleo B
+                0.1,  # Gasóleo C
+                0.1,  # Biodiésel
+                0.1,  # Autogas/GLP
+                0.1,  # GNC
+                0.1,  # Luz
             ],
-            'vivienda': [
-                0.5, # Venta
-                0.5 # Alquiler
-            ],
+            "vivienda": [0.5, 0.5],  # Venta  # Alquiler
         }
         inflationCategory_pct = []
         for num1, num2 in zip(daily_inflations, pesos[sector]):
             inflationCategory_pct.append(num1 * num2)
-        inflationCategory= sum(inflationCategory_pct)
+        inflationCategory = sum(inflationCategory_pct)
 
-        categ_inflations.append({'fecha':date, 'inflation':inflationCategory , 'category': sector})
-    
+        categ_inflations.append(
+            {"fecha": date, "inflation": inflationCategory, "category": sector}
+        )
+
     categ_inflations = pd.DataFrame.from_records(categ_inflations)
 
     return categ_inflations
@@ -160,100 +216,132 @@ def calcCategoriesInflation(inflations_product, sector):
 def calcTotalInflation(inflations_categ):
     total_inflations = []
     for date in inflations_categ.fecha.unique():
-        daily_inflations = inflations_categ[inflations_categ['fecha'] == date]['inflation'].values
+        daily_inflations = inflations_categ[inflations_categ["fecha"] == date][
+            "inflation"
+        ].values
 
-        pesos = [
-            0.36, # Alimentacion
-            0.23, # Energia
-            0.31  # Vivienda
-        ]
+        pesos = [0.36, 0.23, 0.31]  # Alimentacion  # Energia  # Vivienda
         inflationCategory_pct = []
         for num1, num2 in zip(daily_inflations, pesos):
             inflationCategory_pct.append(num1 * num2)
-        inflationCategory= sum(inflationCategory_pct)
-        total_inflations.append({'fecha':date, 'inflation':inflationCategory , 'category': 'total'})
-    
+        inflationCategory = sum(inflationCategory_pct)
+        total_inflations.append(
+            {"fecha": date, "inflation": inflationCategory, "category": "total"}
+        )
+
     total_inflations = pd.DataFrame.from_records(total_inflations)
 
     return total_inflations
 
+
 def getProductPrices():
     prod_prices = {}
 
-    prod_prices['energia'] = pd.json_normalize(getEnergiaJson())
+    prod_prices["energia"] = pd.json_normalize(getEnergiaJson())
     prices_vivienda = pd.json_normalize(getViviendaJson())
-    prod_prices['vivienda'] = prices_vivienda.loc[~((prices_vivienda['fuente'] == 'idealista') & (prices_vivienda['provincia'] == 'Madrid'))].copy()
-    prod_prices['alimentacion'] = pd.json_normalize(getAlimentacionJson())
-    prod_prices['alimentacion']["precio"] = prod_prices['alimentacion']["precio_referencia"] 
+    prod_prices["vivienda"] = prices_vivienda.loc[
+        ~(
+            (prices_vivienda["fuente"] == "idealista")
+            & (prices_vivienda["provincia"] == "Madrid")
+        )
+    ].copy()
+    prod_prices["alimentacion"] = pd.json_normalize(getAlimentacionJson())
+    prod_prices["alimentacion"]["precio"] = prod_prices["alimentacion"][
+        "precio_referencia"
+    ]
 
-    prod_prices['energia'].rename(columns = {'combustible':'producto'}, inplace = True)
-    prod_prices['vivienda'].rename(columns = {'tipo':'producto'}, inplace = True)
-    prod_prices['alimentacion'].rename(columns = {'tienda':'fuente'}, inplace = True)
+    prod_prices["energia"].rename(columns={"combustible": "producto"}, inplace=True)
+    prod_prices["vivienda"].rename(columns={"tipo": "producto"}, inplace=True)
+    prod_prices["alimentacion"].rename(columns={"tienda": "fuente"}, inplace=True)
 
     return prod_prices
+
 
 def getSourcesInflation(sector_df, ago):
     fuente_infl = {}
 
-    fuente_infl['energia'] = calcInflationFuentes(sector_df['energia'], ago)
-    fuente_infl['vivienda'] = calcInflationFuentes(sector_df['vivienda'], ago)
-    fuente_infl['alimentacion'] = calcInflationFuentes(sector_df['alimentacion'], ago)
+    fuente_infl["energia"] = calcInflationFuentes(sector_df["energia"], ago)
+    fuente_infl["vivienda"] = calcInflationFuentes(sector_df["vivienda"], ago)
+    fuente_infl["alimentacion"] = calcInflationFuentes(sector_df["alimentacion"], ago)
 
     return fuente_infl
+
 
 def getProductInflation(sector_df, ago):
     prod_infl = {}
 
     # Normalizar precios de la luz a medias semanales
-    sector_df['energia'] = normalize_luz(sector_df['energia'])
-    sector_df['vivienda'] = preprocess_vivienda(sector_df['vivienda'])
+    sector_df["energia"] = normalize_luz(sector_df["energia"])
+    sector_df["vivienda"] = preprocess_vivienda(sector_df["vivienda"])
 
-    prod_infl['energia'] = calcInflationProds(sector_df['energia'], ago)
-    prod_infl['vivienda'] = calcInflationProds(sector_df['vivienda'], ago)
-    prod_infl['alimentacion'] = calcInflationProds(sector_df['alimentacion'], ago)
+    prod_infl["energia"] = calcInflationProds(sector_df["energia"], ago)
+    prod_infl["vivienda"] = calcInflationProds(sector_df["vivienda"], ago)
+    prod_infl["alimentacion"] = calcInflationProds(sector_df["alimentacion"], ago)
 
     return prod_infl
+
 
 def getCategoriesInflation(sector_df):
     category_infl = {}
 
-    category_infl['energia'] = calcCategoriesInflation(sector_df['energia'], 'energia')
-    category_infl['vivienda'] = calcCategoriesInflation(sector_df['vivienda'], 'vivienda')
-    category_infl['alimentacion'] = calcCategoriesInflation(sector_df['alimentacion'], 'alimentacion')
+    category_infl["energia"] = calcCategoriesInflation(sector_df["energia"], "energia")
+    category_infl["vivienda"] = calcCategoriesInflation(
+        sector_df["vivienda"], "vivienda"
+    )
+    category_infl["alimentacion"] = calcCategoriesInflation(
+        sector_df["alimentacion"], "alimentacion"
+    )
 
     return category_infl
 
+
 def getTotalInflation(categs_df):
-    categories_month_infl = pd.concat([categs_df['energia'], categs_df['vivienda'], categs_df['alimentacion']])
-    categories_month_infl = pd.concat([categories_month_infl, calcTotalInflation(categories_month_infl)])
+    categories_month_infl = pd.concat(
+        [categs_df["energia"], categs_df["vivienda"], categs_df["alimentacion"]]
+    )
+    categories_month_infl = pd.concat(
+        [categories_month_infl, calcTotalInflation(categories_month_infl)]
+    )
     categories_month_infl.sort_values(by="fecha", inplace=True)
     return categories_month_infl
 
 
 def normalize_luz(energy_prices):
     # Function that calculates the updates de electricity price with the mean pf the last 7 days
-    luz_df = energy_prices[energy_prices['producto'] == 'Luz']
-    rolling_mean = luz_df['precio'].rolling(window=7, min_periods=1).mean()
+    luz_df = energy_prices[energy_prices["producto"] == "Luz"]
+    rolling_mean = luz_df["precio"].rolling(window=7, min_periods=1).mean()
 
     # Update the 'precio' column in the original dataframe with the rolling mean values
-    energy_prices.loc[energy_prices['producto'] == 'Luz', 'precio'] = rolling_mean
+    energy_prices.loc[energy_prices["producto"] == "Luz", "precio"] = rolling_mean
 
     return energy_prices
 
+
 def preprocess_vivienda(vivienda_prices):
-    vivienda_prices = vivienda_prices[vivienda_prices["provincia"] == 'España']
+    vivienda_prices = vivienda_prices[vivienda_prices["provincia"] == "España"]
     return vivienda_prices
 
-# # get product prices
-# prod_prices  = getProductPrices()
 
-# # Get inflation per product
+def getCategoriesInflation():
+    """
+    Calculate inflation for each category in the given sectors.
 
-# prod_infl = getProductInflation(prod_prices, 30) 
+    Args:
+        sector_df (Dict[str, pd.DataFrame]): A dictionary containing DataFrames with product prices
+                                             for 'energia', 'vivienda', and 'alimentacion' sectors.
 
-# # Get inflation per category 
-# category_infl = getCategoriesInflation(prod_infl)
+    Returns:
+        Dict[str, pd.DataFrame]: A dictionary containing DataFrames with inflation data for each category
+                                 in the 'energia', 'vivienda', and 'alimentacion' sectors.
+    """
 
+    # Read data from MongoDB collections
+    myclient = pymongo.MongoClient(
+        "mongodb+srv://cprietof:INcamachaja9@chainflation-east.eoueeme.mongodb.net/?retryWrites=true&w=majority"
+    )
+    mydb = myclient[DATABASE]
 
-# # Get total inflation
-# total = getTotalInflation(category_infl)
+    collection = "YoY_inflation_category"
+
+    YoY_inflation_category_df = pd.json_normalize(list(mydb[collection].find()))
+    return YoY_inflation_category_df
